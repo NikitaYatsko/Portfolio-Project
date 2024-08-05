f8a469d3f025c69950f97ebfc972f56c488d7c7a
Nikita <archon04@mail.ru>
2024-08-05 22:20:59 +0300

    add beautiful animation to modal

--- js/utils/modal.js
+++ js/utils/modal.js
@@ -1,12 +1,12 @@
-export function modalDialog(){
+export function modalDialog() {
     const modalTrigger = document.querySelectorAll('.modal_trigger');
-    const modalOverlay = document.querySelector('.modal_overlay')
-    modalTrigger.forEach(item =>{
-        item.addEventListener('click', openModal)
-    })
+    const modalOverlay = document.querySelector('.modal_overlay');
 
-    document.addEventListener('click', closeModal)
+    modalTrigger.forEach(item => {
+        item.addEventListener('click', openModal);
+    });
 
+    document.addEventListener('click', closeModal);
 
     const modalTimer = setTimeout(function () {
         openModal();
@@ -14,15 +14,22 @@ export function modalDialog(){
 
     function openModal() {
         modalOverlay.classList.remove('hide');
+        setTimeout(() => {
+            modalOverlay.classList.add('show');
+        }, 10);
         document.body.style.overflowY = 'hidden';
         modalOverlay.style.backgroundColor = 'rgba(0, 0, 0, 0.80)';
-        clearInterval(modalTimer);
+        clearTimeout(modalTimer);
     }
 
     function closeModal(event) {
         if (event.target.classList.contains('modal_overlay')) {
-            modalOverlay.classList.add('hide');
+            modalOverlay.classList.remove('show');
             document.body.style.overflowY = 'scroll';
+            setTimeout(() => {
+                modalOverlay.classList.add('hide');
+                modalOverlay.style.backgroundColor = 'transparent';
+            }, 300);
         }
     }
 }
